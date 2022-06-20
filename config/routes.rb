d26842d589821d59ba8aa2fbd424f5bc4a1e4297
risona0909 <105469316+risona0909@users.noreply.github.com>
2022-06-20 19:30:26 +0000

    [Finish]アプリケーションを完成させよう7章

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  #.../lists/1や.../lists/3に該当する
-  get 'lists/:id' => 'lists#show' , as: 'list'
+
+  get 'top' => 'homes#top'
+  resources :lists
+
 end
